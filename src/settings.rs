003dbf98d67c1afc02c755e44d73ed7839deaa3b
Yomguithereal <guillaumeplique@gmail.com>
2025-11-26 14:57:21 +0100

    Generic float precision

--- src/settings.rs
+++ src/settings.rs
@@ -1,37 +1,55 @@
+use num::Float;
+
 #[derive(Debug, Clone)]
-pub struct Settings {
+pub struct Settings<F: Float> {
     lin_log_mode: bool,
-    edge_weight_influence: f64,
-    gravity: f64,
+    edge_weight_influence: F,
+    gravity: F,
     strong_gravity_mode: bool,
     outbound_attraction_distribution: bool,
-    scaling_ratio: f64,
-    slow_down: f64
+    scaling_ratio: F,
+    slow_down: F,
 }
 
-impl Default for Settings {
+impl<F: Float> Default for Settings<F> {
     fn default() -> Self {
         Self {
             lin_log_mode: false,
-            edge_weight_influence: 1.0,
-            gravity: 1.0,
+            edge_weight_influence: F::one(),
+            gravity: F::one(),
             strong_gravity_mode: false,
             outbound_attraction_distribution: false,
-            scaling_ratio: 1.0,
-            slow_down: 1.0
+            scaling_ratio: F::one(),
+            slow_down: F::one(),
         }
     }
 }
 
-impl Settings {
+impl<F: Float> Settings<F> {
     pub fn from_graph_order(order: usize) -> Self {
         let mut settings = Self::default();
 
         settings.strong_gravity_mode = true;
-        settings.gravity = 0.05;
-        settings.scaling_ratio = 10.0;
-        settings.slow_down = 1.0 + (order as f64).ln();
+        settings.gravity = F::from(0.05).unwrap();
+        settings.scaling_ratio = F::from(10.0).unwrap();
+        settings.slow_down = F::one() + F::from((order as f64).ln()).unwrap();
 
         settings
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_from_graph_order() {
+        let settings = Settings::<f32>::from_graph_order(32);
+
+        assert_eq!(settings.slow_down, 4.465736);
+
+        let settings = Settings::<f64>::from_graph_order(32);
+
+        assert_eq!(settings.slow_down, 4.465735902799727);
     }
 }
