854b3f80040c335c5a549a4d66611136a1c7c1b3
Yomguithereal <guillaumeplique@gmail.com>
2026-05-20 10:10:55 +0200

    Dropping `num` crate in favor of `num-traits` Fix #9

--- src/traits.rs
+++ src/traits.rs
@@ -1,6 +1,6 @@
 use std::ops::{AddAssign, DivAssign, SubAssign};
 
-use num::Float as NumFloat;
+use num_traits::Float as NumFloat;
 
 pub trait Float: NumFloat + SubAssign + AddAssign + DivAssign {}
 impl<T: NumFloat + SubAssign + AddAssign + DivAssign> Float for T {}
