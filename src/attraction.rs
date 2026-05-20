50868fb6153c545342c412267a5ef428692135ae
Yomguithereal <guillaumeplique@gmail.com>
2026-05-20 18:16:10 +0200

    Tighter attraction loop

--- src/attraction.rs
+++ src/attraction.rs
@@ -7,11 +7,8 @@ pub fn apply_attraction<F: Float>(
     edges: &[(usize, usize, F)],
     out: &mut [F],
 ) {
-    let coefficient = F::one();
-
     for (source, target, weight) in edges {
-        let ewc = weight.powf(settings.edge_weight_influence);
-        let factor = -coefficient * ewc;
+        let factor = -weight.powf(settings.edge_weight_influence);
 
         let x_dist = nodes[source * 3] - nodes[target * 3];
         let y_dist = nodes[source * 3 + 1] - nodes[target * 3 + 1];
