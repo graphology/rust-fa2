595c8492e6b163c12bc2c696575a68b86b72607c
Yomguithereal <guillaumeplique@gmail.com>
2026-05-22 14:43:52 +0200

    Improving cluster generation script

--- scripts/generate/index.js
+++ scripts/generate/index.js
@@ -1,10 +1,19 @@
 const Graph = require('graphology');
 const clusters = require('graphology-generators/random/clusters');
+const seedrandom = require('seedrandom');
 
-const graph = clusters(Graph, {order: 10_000 * 5, size: 50_000 * 5, clusters: 5, clusterDensity: 0.7});
+const rng = seedrandom('rust-fa2');
+
+const graph = clusters(Graph, {order: 10_000 * 5, size: 50_000 * 5, clusters: 5, clusterDensity: 0.7, rng});
 
 console.log("source,target");
 
 graph.forEachEdge((_edge, _attr, source, target) => {
     console.log(`${source},${target}`);
 });
+
+// console.log("node,cluster");
+
+// graph.forEachNode((node, attr) => {
+//     console.log(`${node},${attr.cluster}`);
+// })
