6defb649a8daa2ac8fe5345dd309217180a45526
Yomguithereal <guillaumeplique@gmail.com>
2026-05-29 16:32:19 +0200

    Working on rasterizer

--- scripts/generate/draw.js
+++ scripts/generate/draw.js
@@ -2,6 +2,12 @@
 const fs = require('fs');
 const Graph = require('graphology');
 const {renderToPNG} = require('graphology-canvas/node');
+const {scaleLog} = require('d3-scale');
+
+// 8192 for readable raster attempt
+const SIZE = 2048;
+
+let sizeScale = scaleLog([1, 10292203], [1, SIZE * 0.001]);
 
 const DATA = fs.readFileSync(process.argv[2], 'utf-8');
 
@@ -9,7 +15,7 @@ const graph = new Graph();
 
 let i = -1;
 
-DATA.split("\n").forEach(line => {
+DATA.split("\n").slice(0, 1_000_000).forEach(line => {
     if (i == -1) {
         i++;
         return;
@@ -19,11 +25,14 @@ DATA.split("\n").forEach(line => {
         return;
     }
 
-    let [x, y] = line.split(',');
+    let [label, degree, x, y] = line.split(',');
 
-    graph.addNode(i++, {x: +x, y: +y});
+    degree = +degree || 1;
+    let size = sizeScale(degree);
+
+    graph.addNode(i++, {x: +x, y: +y, size, label});
 });
 
 console.log(graph.order);
 
-renderToPNG(graph, "graph.png", {height: 16384, width: 16384}, () => console.log("done!"));
+renderToPNG(graph, "graph.png", {width: SIZE}, () => console.log("done!"));
